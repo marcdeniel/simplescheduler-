3d46b4cad2e72c5071a93f06684182ee1450ef43
marcdeniel <marcdeniel@yahoo.com>
2024-07-02 16:59:25 -0700

    Create appointments.py

---
+++ appointments.py
@@ -0,0 +1,8 @@
+#simplescheduler/appointments.py
+appointments[]
+def addappointment(appointment):
+  appointments.append(appointment) #add an appointment to the list
+def viewappointments()
+  print("Appointments:")
+  for appointment in appointments
+    print(f"-{appointment}")
